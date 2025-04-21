48e89f4868314096ea0df74165b051f3596fecfc
Sebastian Rodriguez <srodriguez@canvia.com>
2025-04-21 09:49:30 -0500

    Pantalla data

--- src/app/pages/pension-appraiser/pension-appraiser.component.ts
+++ src/app/pages/pension-appraiser/pension-appraiser.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MyDataComponent } from './components/my-data/my-data.component';
 import { CommonModule } from '@angular/common';
+import { MySavingsComponent } from './components/my-savings/my-savings.component';
 
 @Component({
   selector: 'app-pension-appraiser',
-  imports:[MyDataComponent,CommonModule],
+  imports:[MyDataComponent,CommonModule,MySavingsComponent],
   templateUrl: './pension-appraiser.component.html',
   styleUrl: './pension-appraiser.component.scss'
 })
