86bf60eadcf836c2ec4880544443a84228e0d7d3
christian <christian_gcamus@hotmail.com>
2025-04-24 15:24:08 -0500

    feat: add component contact system

---
+++ src/app/pages/pension-appraiser/components/contact-system/contact-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-system',
+  imports: [],
+  templateUrl: './contact-system.component.html',
+  styleUrl: './contact-system.component.scss'
+})
+export class ContactSystemComponent {
+
+}
--- src/app/pages/pension-appraiser/pension-appraiser.component.ts
+++ src/app/pages/pension-appraiser/pension-appraiser.component.ts
@@ -5,10 +5,19 @@ import { MySavingsComponent } from './components/my-savings/my-savings.component
 import { MyFamilyComponent } from './components/my-family/my-family.component';
 import { MyProfitabilityComponent } from './components/my-profitability/my-profitability.component';
 import { MyResultComponent } from './components/my-result/my-result.component';
+import { ContactSystemComponent } from './components/contact-system/contact-system.component';
 
 @Component({
   selector: 'app-pension-appraiser',
-  imports:[MyDataComponent,CommonModule,MySavingsComponent,MyFamilyComponent,MyProfitabilityComponent,MyResultComponent],
+  imports:[
+    MyDataComponent,
+    CommonModule,
+    MySavingsComponent,
+    MyFamilyComponent,
+    MyProfitabilityComponent,
+    MyResultComponent,
+    ContactSystemComponent
+  ],
   templateUrl: './pension-appraiser.component.html',
   styleUrl: './pension-appraiser.component.scss'
 })
