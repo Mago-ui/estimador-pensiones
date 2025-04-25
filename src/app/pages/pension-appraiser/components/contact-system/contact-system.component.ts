5b4430d61fe27b65ad75e07665ca50f07f909223
Sebastian Rodriguez <srodriguez@canvia.com>
2025-04-25 12:17:01 -0500

    Sistema de contacto formulario

--- src/app/pages/pension-appraiser/components/contact-system/contact-system.component.ts
+++ src/app/pages/pension-appraiser/components/contact-system/contact-system.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-contact-system',
-  imports: [],
+  imports: [NzSelectModule,NzDatePickerModule,NzButtonModule, NzModalModule],
   templateUrl: './contact-system.component.html',
   styleUrl: './contact-system.component.scss'
 })
-export class ContactSystemComponent {
+export class ContactSystemComponent implements OnInit{
+
+  isVisible = false;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
 
 }
